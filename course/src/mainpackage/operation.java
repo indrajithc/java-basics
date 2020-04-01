f475c16d3af3487fff33f743732a0110cfc93b55
Indrajith C <indranspeaks@gmail.com>
2020-04-01 19:52:50 +0530

    operations sample

---
+++ course/src/mainpackage/operation.java
@@ -0,0 +1,12 @@
+package course.src.mainpackage;
+
+public class operation {
+
+    public static void main(String[] args) {
+        
+        int o1 = 10;
+        int o2 = 20;
+        int o3 = 30;
+        System.out.println("output o1 + o2 * o3  : " +( o1 + o2 * o3));
+    }
+}
