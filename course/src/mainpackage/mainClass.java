ba66163bdaaee2d79f7bcbf15c0606244f403d77
Indrajith C <indranspeaks@gmail.com>
2020-04-01 14:04:13 +0530

    Read user details

--- course/src/mainpackage/mainClass.java
+++ course/src/mainpackage/mainClass.java
@@ -4,10 +4,26 @@ import java.util.Scanner;
 
 public class mainClass {
     static Scanner scanner = new Scanner(System.in);
+    static Scanner reader = new Scanner(System.in);
     public static void main( String [] args) {
-        String inputText = "";
-        System.out.println("Enter something ");
-        inputText = scanner.nextLine();
-        System.out.println( inputText);
+        
+        System.out.print("\n Enter name : "); 
+        String name = scanner.nextLine();
+        System.out.print("Enter age : ");
+        int age = scanner.nextInt();
+        System.out.print("Enter Salary : ");
+        double salary = scanner.nextDouble();
+        System.out.print("Enter Gender : ");
+        String tempInput = reader.nextLine();
+        char gender = tempInput.charAt(0);
+
+        System.out.println("\n\n Your details ");
+        System.out.println(" ------------------------------- ");
+        System.out.println("Name  : " + name);
+        System.out.println("Age  : " + age);
+        System.out.println("Salary  : " + salary);
+        System.out.println("Gender  : " + gender);
+
+
     }
 }
