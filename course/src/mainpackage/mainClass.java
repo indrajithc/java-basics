83f0785dd7feb14b9665226fb2d8c81a50675fc3
Indrajith C <indranspeaks@gmail.com>
2020-04-01 13:51:27 +0530

    basic string reading

--- course/src/mainpackage/mainClass.java
+++ course/src/mainpackage/mainClass.java
@@ -1,7 +1,13 @@
 package course.src.mainpackage;
 
+import java.util.Scanner;
+
 public class mainClass {
+    static Scanner scanner = new Scanner(System.in);
     public static void main( String [] args) {
-        System.out.println("hello Indan");
+        String inputText = "";
+        System.out.println("Enter something ");
+        inputText = scanner.nextLine();
+        System.out.println( inputText);
     }
 }
