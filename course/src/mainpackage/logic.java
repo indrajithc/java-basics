cd35630c5c92fef5789ada39b0a661b0b3bedcde
Indrajith C <indranspeaks@gmail.com>
2020-04-01 20:03:05 +0530

    logic operations

---
+++ course/src/mainpackage/logic.java
@@ -0,0 +1,9 @@
+package course.src.mainpackage;
+
+public class logic { 
+    public static void main(String[] args) {
+        if( 23 > 2) { 
+            System.out.println(" 23 is greater than 3");
+        }
+    }
+}
