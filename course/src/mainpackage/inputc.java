d0cdb3057fd410bce08c5bfbf3416ace959240ed
Indrajith C <indranspeaks@gmail.com>
2020-04-01 19:29:00 +0530

    Reading args

---
+++ course/src/mainpackage/inputc.java
@@ -0,0 +1,9 @@
+package course.src.mainpackage;
+
+public class inputc {
+
+    public static void main( String[] args) {
+        System.out.println("Args are \n --------------");
+        System.out.println(args[0]);
+    }
+}
