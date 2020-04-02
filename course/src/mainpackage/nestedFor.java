006aece6816277e4c09c9190a426c552abb897cd
Indrajith C <indranspeaks@gmail.com>
2020-04-02 14:20:02 +0530

    nested for loop

---
+++ course/src/mainpackage/nestedFor.java
@@ -0,0 +1,29 @@
+package course.src.mainpackage;
+
+public class nestedFor {
+   public static void main(String[] args) {
+       for( int o = 0; o <  10; o++) {
+           for( int p=0; p< 10; p++) {
+               System.out.print("*");
+           }
+           System.out.println("");
+       }
+
+       for( int o = 0; o <  10; o++) {
+        for( int p=0; p< o; p++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+
+    for( int o = 0; o <=  9; o++) {
+        for( int p=0; p<= o; p++) {
+            if( p == o)
+            System.out.print("*");
+            else
+            System.out.print(" ");
+        }
+        System.out.println("");
+    }
+   }
+}
