171b0d2e02a032f98b4b235783b5c7bd8d542b82
Indrajith C <indranspeaks@gmail.com>
2020-04-02 14:26:27 +0530

    break and continue in loop

--- course/src/mainpackage/nestedFor.java
+++ course/src/mainpackage/nestedFor.java
@@ -24,6 +24,29 @@ public class nestedFor {
             System.out.print(" ");
         }
         System.out.println("");
+    } 
+
+   for( int o = 0; o <=  9; o++) {
+        for( int p=0; p<= 9; p++) {
+            if( p == 4) {
+                break; 
+                } 
+            System.out.print(" ["+ o + p +"] "); 
+        }
+        System.out.println("");
     }
-   }
+
+
+    for( int o = 0; o <=  9; o++) {
+        for( int p=0; p<= 9; p++) {
+            if( p == 4) {
+                // break; 
+                continue;
+                } 
+            System.out.print(" ["+ o + p +"] "); 
+        }
+        System.out.println("");
+    }
+}
+
 }
