38f5a25dd77a9cf6327aea69465758d14e1345ab
Indrajith C <indranspeaks@gmail.com>
2020-04-02 14:19:52 +0530

    simple for loop

---
+++ course/src/mainpackage/simpleFor.java
@@ -0,0 +1,10 @@
+package course.src.mainpackage;
+
+public class simpleFor {
+  public static void main(String[] args) {
+      int o = 0;
+      for( o = 0; o < 10 ; o++) {
+          System.out.println("hello indran");
+      }
+  }
+}   
