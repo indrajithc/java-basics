c30558a0c5c565b0c27edc4be30c9676ace35733
Indrajith C <indranspeaks@gmail.com>
2020-04-03 18:50:57 +0530

    multiple threads

--- course/src/subpackagefour/MyThread.java
+++ course/src/subpackagefour/MyThread.java
@@ -2,12 +2,18 @@ package course.src.subpackagefour;
 
 public class MyThread extends Thread {
     boolean isRunning = true;
+    String name = "";
+
+    public MyThread ( String name ) {
+        this.name = name;
+    }
+
      @Override
     public void run() {
 
         int count = 0;
         while( isRunning ) {
-            System.out.println(count);
+            System.out.println( "[ " + name + " ]" + count);
             count ++;
             try {
 				Thread.sleep(1000);
--- course/src/subpackagefour/MyThreadClass.java
+++ course/src/subpackagefour/MyThreadClass.java
@@ -5,8 +5,12 @@ public class MyThreadClass {
     public static void main(String[] args) {
         System.out.println("Thread example");
 
-        MyThread myThread = new MyThread();
+        MyThread myThread = new MyThread("T");
+        MyThread myThread1 = new MyThread("X");
+        MyThread myThread2 = new MyThread("Y");
         myThread.start();
+        myThread1.start();
+        myThread2.start();
 
         try {
 			Thread.sleep(6000);
@@ -16,6 +20,17 @@ public class MyThreadClass {
         }
         
         myThread.isRunning = false;
+
+        try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+        }
+        myThread1.isRunning = false;
+        myThread2.isRunning = false;
+        myThread.isRunning = true; // not worked
+        
        
     }
 }
