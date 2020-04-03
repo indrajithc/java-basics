a731de4ed1a0c344403c5b86b2d49e8c6a706475
Indrajith C <indranspeaks@gmail.com>
2020-04-03 12:49:46 +0530

    Generic function

--- course/src/subpackageone/GenericClass.java
+++ course/src/subpackageone/GenericClass.java
@@ -6,8 +6,8 @@ public class GenericClass {
     public static void main(String[] args) {
         
         String[] a1 = { "Aby", "Ciby", "Baby"};
-        int[] a2 = { 1, 2, 3, 45, 6};
-        double[] a3 = { 1.3, 4.3, 5.1 };
+        Integer[] a2 = { 1, 2, 3, 45, 6};
+        Double[] a3 = { 1.3, 4.3, 5.1 };
 
         print( a1);
         print( a2);
@@ -15,20 +15,9 @@ public class GenericClass {
 
     }
 
-   static void print( String[] arr) {
-        for( String value : arr) {
+   static <T>void print( T[] arr) {
+        for( T value : arr) {
             System.out.println( value);
         }
-    }
-
-   static void print( int[] arr) {
-        for( int value : arr) {
-            System.out.println( value);
-        }
-    }
-    static void print( double[] arr) {
-        for( double value : arr) {
-            System.out.println( value);
-        }
-    }
+    } 
 }
