f3c9642d436e19b96ba7c7bd853729f876b45679
Indrajith C <indranspeaks@gmail.com>
2020-04-03 13:28:21 +0530

    Read and write into file

--- course/src/subpackagetwo/SimpleFileClass.java
+++ course/src/subpackagetwo/SimpleFileClass.java
@@ -1,15 +1,35 @@
 package course.src.subpackagetwo;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class SimpleFileClass {
-
+    static String filePath = "file.txt";
     public static void main(String[] args) {
-        String filePath = "file.txt";
+        
+        writeToFile("Hello from java world");
+        readFromFile();
+    }
+
+    static void writeToFile( String string ) {
         try {
             FileWriter fileWriter =  new FileWriter(filePath);
-            fileWriter.write("Welcome back to java world");
+            fileWriter.write(string);
             fileWriter.close();
+        } catch( Exception e) {
+            System.out.println("\n\nError : " + e.getMessage());
+        }
+    }
+    
+    static void readFromFile( ) {
+        try {
+            FileReader fileReader =  new FileReader(filePath); 
+            int c;
+            while( (c = fileReader.read())!= -1) {
+                System.out.print(( char) c);
+            }
+            System.out.println();
+            fileReader.close();
         } catch( Exception e) {
             System.out.println("\n\nError : " + e.getMessage());
         }
